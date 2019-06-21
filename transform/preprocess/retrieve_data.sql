24751e641a6d0a10a2fb26ef570523a67df63a96
WillianFuks <willian.fuks@gmail.com>
2019-06-21 08:55:35 -0300

    dataflow is working

--- transform/preprocess/retrieve_data.sql
+++ transform/preprocess/retrieve_data.sql
@@ -2,10 +2,9 @@ SELECT
   date,
   customer_id,
   --hits
-  ARRAY(SELECT AS STRUCT action AS action, productSku AS productSku FROM UNNEST(hits)) AS hits -- cheatting!
+  ARRAY(SELECT AS STRUCT action AS action, productSku AS productSku FROM UNNEST(hits) WHERE action IN ('Browsed', 'AddedToBasket')) AS hits -- cheatting!
 FROM `{project_id}.papis19.dafiti_data`
 WHERE TRUE
   AND ARRAY_LENGTH(hits) > 1
   AND EXISTS(SELECT 1 FROM UNNEST(hits) WHERE action = 'Browsed')
   AND date BETWEEN '{init_date}' AND '{end_date}'
-LIMIT 10
